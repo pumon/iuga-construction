389a2b498c3acf784417a2fce22fe34530d9c1b2
pumon <punith345@gmail.com>
2019-04-08 15:12:28 +0530

    admin added with backend

--- config.php
+++ config.php
@@ -3,7 +3,5 @@ include("database.php");
 $conn = new mysqli($host, $user, $password, $database);
 if($conn->connect_errno > 0){
     die('Unable to connect to database'. $conn->connect_error);
-}else{
-    echo "Connected";
 }
 ?>
--- quote.php
+++ quote.php
@@ -8,10 +8,10 @@ if(isset($_POST['email'])&& $_POST['email']!=null){
     $sql = "INSERT INTO quote VALUES ('$name','$email','$message')";
     $res=mysqli_query($conn, $sql);
     if(isset($res)){
-        echo "<script>alert('Records inserted successfully.');window.location.href='index.html';</script>";
+        echo "<script>alert('Submitted successfully');window.location.href='index.html';</script>";
 
     } else{
-        echo "<script>alert('Records not successfully.');window.location.href='index.html';</script>;";
+        echo "<script>alert('Failed');window.location.href='index.html';</script>;";
 
     }
 
